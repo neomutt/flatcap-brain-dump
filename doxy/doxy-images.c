7335736a92f26e8e4d22d33bc1d36cf0b40ba54e
Richard Russon <rich@flatcap.org>
2021-10-28 12:12:16 +0100

    doxy

---
+++ doxy/doxy-images.c
@@ -0,0 +1,9 @@
+ * <div style="float: right">
+ * <b>Schematic</b><br><img width="600" src="dlg-main.svg"><br><br>
+ * <b>Window Layout</b><br><img width="600" src="dlg-main2.svg">
+ * </div>
+
+ * <div class="screens">
+ * Schematic <img src="dlg-main.svg"> 
+ * Window Layout <img src="dlg-main2.svg"> 
+ * </div>
